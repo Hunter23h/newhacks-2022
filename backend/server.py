16161843e1801674da382f1e333bc4ced17b317f
Samuel Emard-Thibault <sam5thibault@gmail.com>
2022-11-05 13:59:58 -0400

    fix cors error

--- backend/server.py
+++ backend/server.py
@@ -1,10 +1,16 @@
 from flask import Flask
+from flask_cors import CORS, cross_origin
+
 
 app = Flask(__name__)
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
 
 @app.route("/getdata", methods=["POST" ,"GET"])
+@cross_origin()
 def getdata():
     return {"Message" : "it works yay"}
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
