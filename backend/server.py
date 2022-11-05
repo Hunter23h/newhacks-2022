1ada7d9b7cea0bcd888db7e6e18406e96e7e5e87
Samuel Emard-Thibault <sam5thibault@gmail.com>
2022-11-05 14:37:49 -0400

    l

--- backend/server.py
+++ backend/server.py
@@ -1,15 +1,29 @@
-from flask import Flask
+from flask import Flask, request
 from flask_cors import CORS, cross_origin
+from citation import Citation
+from summarizer import Summarizer
 
 
 app = Flask(__name__)
 cors = CORS(app)
 app.config['CORS_HEADERS'] = 'Content-Type'
 
-@app.route("/getdata", methods=["POST" ,"GET"])
+@app.route("/getdata", methods=["POST"])
 @cross_origin()
 def getdata():
-    return {"Message" : "it works yay"}
+
+    client_data = request.json
+    print(client_data)
+
+    url = client_data['link']
+    citation = Citation(url).main() 
+    summary = Summarizer(url).main() 
+
+    return {
+        "summary" : summary,
+        "score" : 5,
+        "MLA" : citation
+    }
 
 
 if __name__ == "__main__":
