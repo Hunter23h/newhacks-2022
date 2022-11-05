e16a269a9ea9604c4a492c0c2f25b05363a620f2
LoganHoogendijk <logan.hoogendijk@gmail.com>
2022-11-05 14:31:58 -0400

    fixed reliability

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,10 @@
 from citation import Citation
 from summarizer import Summarizer
+from reliability import Reliability
 
-url = 'https://en.wikipedia.org/wiki/Artificial_intelligence'
+url = 'https://www.ibm.com/cloud/learn/what-is-artificial-intelligence'#'https://en.wikipedia.org/wiki/Artificial_intelligence'
 
 Citation(url).main()
 Summarizer(url).main()
+date1 = Citation(url).date_finder()
+Reliability(url, date1).main()
