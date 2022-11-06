0d19d9079691ac3caac96e8fa327ba04a10e084c
Samuel Emard-Thibault <sam5thibault@gmail.com>
2022-11-05 20:11:57 -0400

    fix frontend

--- backend/redeploy_site.sh
+++ backend/redeploy_site.sh
@@ -9,5 +9,13 @@ sudo docker container stop linkhack
 sudo docker container rm linkhack
 sudo docker image build -t linkhack_image .
 
+cd ../
+cd frontend
+
+sudo docker container stop linkhack_frontend
+sudo docker container rm linkhack_frontend
+sudo docker image build -t linkhack_image_frontend .
+
 echo "Re-building and spinning up application..."
 sudo docker run --name linkhack -p 5001:5000 -d linkhack_image
+sudo docker run -d --name linkhack_frontend -it --rm -v ${PWD}:/app -v /app/node_modules -p 3000:3000 -e CHOKIDAR_USEPOLLING=true linkhack_image_frontend
